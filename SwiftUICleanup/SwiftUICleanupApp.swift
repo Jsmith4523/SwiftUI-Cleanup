dd88cd7a0e38276030f523f7fae3aea5b0468ab1
Jaylen Smith <kobe26945@icloud.com>
2023-03-08 12:11:05 -0500

    Initial Commit

---
+++ SwiftUICleanup/SwiftUICleanupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICleanupApp.swift
+//  SwiftUICleanup
+//
+//  Created by Jaylen Smith on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICleanupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
